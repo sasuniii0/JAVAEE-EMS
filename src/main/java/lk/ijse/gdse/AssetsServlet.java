ad5b307186857f095a94ea9fab9f37b3189203d5
sasuniii0 <sasuniwijerathne@gmail.com>
2025-06-12 01:28:02 +0530

    complete

--- src/main/java/lk/ijse/gdse/AssetsServlet.java
+++ src/main/java/lk/ijse/gdse/AssetsServlet.java
@@ -1,4 +1,36 @@
 package lk.ijse.gdse;
 
-public class AssetsServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@WebServlet("/assets/*")
+public class AssetsServlet extends HttpServlet {
+    private final String assets = "C:\\Users\\User\\OneDrive\\Documents\\AAD\\JAVAEE\\WORK\\EMS-FN\\assets";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String filename = req.getPathInfo();
+        if (filename == null || filename.isEmpty() || filename.equals("/")) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "File not found");
+            return;
+        }
+        File file = new File(assets + filename);
+        if (!file.exists()) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "File not found");
+            return;
+        }
+       String type = getServletContext().getMimeType(file.getName());
+        if (type == null) {
+            type = "application/octet-stream";
+        }
+        resp.setContentType(type);
+        Files.copy(file.toPath(), resp.getOutputStream());
+    }
 }
